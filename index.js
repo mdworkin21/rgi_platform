415ee482639b55dcf3cb5c8f3c904a6c31fae01d
Matt Dworkin <mdworkin21@gmail.com>
2019-03-08 15:03:24 -0500

    heroku deployment troubleshoot

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {db} = require('./server/db/models')
 
 // Syncs with DB and listens for connections on host and port
 // Put in {force: true} (to db.sync()) to update heroku db
-db.sync()
+db.sync({force: true} )
   .then(() => {
     console.log('db synced.')
     app.listen(PORT, () => {
