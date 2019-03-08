4de37472be1c5fb9aad5c4581ee40c63ebf817e4
Matt Dworkin <mdworkin21@gmail.com>
2019-03-08 15:13:00 -0500

    bugz

--- client/redux/store/index.js
+++ client/redux/store/index.js
@@ -3,7 +3,6 @@ import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
 import placeHolder from '../reducers/placeHolder'
 
-
 //Redux Devtools 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 //Combines Reducers
