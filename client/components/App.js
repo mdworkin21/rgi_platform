942418fb3ac0c07aaac66e61791e3f91fe11d90c
Matt Dworkin <mdworkin21@gmail.com>
2019-03-08 14:55:45 -0500

    basic structure set up, server live, etc

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import {Provider} from 'react-redux'
 import store from '../redux/store'
-import Canvas from './Canvas'
 
 //When you add redux, you'll need to add 
 const App = () => {
 return (
   <Provider store={store}>
-    <Canvas />
+  <div>TESTING TESTING</div>
   </Provider>
   )
 }
