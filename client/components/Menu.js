3e150176eeec9ba3d4ea216ba533ad2891c77285
Matt Dworkin <mdworkin21@gmail.com>
2019-03-09 17:45:29 -0500

    put welcome message on menu bar after login

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-
-const Menu = () => {
+const Menu = (props) => {
+  let welcomeMessage = props.user.userName ? `Welcome, ${props.user.userName}` : ''
   return (
-    <div>MENU</div>
-    // <div>Welcome, {this.props.user}</div>
+    <div id="menu-container">
+      <div>MENU</div>
+      <div>{welcomeMessage}</div>
+    </div>
   )
 }
 
-export default Menu
+const mapStateToProps = (state) => {
+  return {
+    user: state.userReducer.user
+  }
+}
+
+export default connect(mapStateToProps)(Menu)
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {db} = require('./server/db/models')
 
 // Syncs with DB and listens for connections on host and port
 // Put in {force: true} (to db.sync()) to update heroku db
-db.sync({force:true})
+db.sync()
   .then(() => {
     console.log('db synced.')
     app.listen(PORT, () => {
