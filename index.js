9877dee5efb85ab5376384e1cf22aa38be320d77
Matt Dworkin <mdworkin21@gmail.com>
2019-03-15 12:37:30 -0400

    still testing heroku seed

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const app = require('./server')
 const PORT = process.env.PORT || 3000
 const {db} = require('./server/db/models')
-const {runSeed} = require('./scripts/seed')
+const {seed} = require('./scripts/seed')
 
 // Syncs with DB and listens for connections on host and port
 // Put in {force: true} (to db.sync()) to update heroku db
 //Don't use in production, dangerous!
-//To seed db with initial admin, put this line after db.sync line
 db.sync({force: true})
-  .then(() => runSeed())
+  .then(() => seed())
   .then(() => {
     console.log('db synced.')
     app.listen(PORT, () => {
