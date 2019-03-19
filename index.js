63034fe499944107a32d004a2fe3230d52eefb00
Matt Dworkin <mdworkin21@gmail.com>
2019-03-19 16:10:35 -0400

    trying to fix deploy issue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const seed = require('./scripts/seed')
 // Syncs with DB and listens for connections on host and port
 // Put in {force: true} (to db.sync()) to update heroku db
 //Don't use in production, dangerous!
-db.sync() 
+db.sync({force: true}) 
   .then(() => seed()) //uncomment this line for initial seeding comment out/delete after
   .then(() => {
     console.log('db synced.')
