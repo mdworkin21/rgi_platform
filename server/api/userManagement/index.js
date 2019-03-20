738881f4de68850f842a712a7bb7d8e93ede781a
Matt Dworkin <mdworkin21@gmail.com>
2019-03-20 08:29:13 -0400

    heroku bug

--- server/api/userManagement/index.js
+++ server/api/userManagement/index.js
@@ -1,7 +1,24 @@
 const router = require('express').Router()
 
+const adminCheck = async (req, res, next) => {
+  try{
+    let userIsAdmin = await User.findOne({
+      where: {
+        id: req.session.passport.user
+      }
+    })
+    if (userIsAdmin.isAdmin){
+      next()
+    } else {
+      res.sendStatus(401)
+    }
+  } catch(err){
+    next(err)
+  }
+}
+
 //Granular Routes
-router.use('/admin', require('./admin'))
+router.use('/admin', adminCheck, require('./admin'))
 
 
 //Handles 404 Errors
