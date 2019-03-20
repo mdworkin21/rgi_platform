40e836cb155f9f171b94826abd6ac12892873d36
Matt Dworkin <mdworkin21@gmail.com>
2019-03-20 08:46:00 -0400

    heroku bug

--- server/api/userManagement/admin.js
+++ server/api/userManagement/admin.js
@@ -18,7 +18,7 @@ const adminCheck = async (req, res, next) => {
   }
 }
 
-router.get('/getUsers', adminCheck, async(req, res, next) => {
+router.get('/getUsers', async(req, res, next) => {
   try{
     console.log('GMMMMMMM', req)
     const allUsers = await User.findAll()
