f688e294bcd5df83a4783b985f60d094e4ca4a04
Matt Dworkin <mdworkin21@gmail.com>
2019-07-05 17:00:11 -0400

    added a utilities folder and file in server/campaign management

--- server/api/campaignManagement/taboola.js
+++ server/api/campaignManagement/taboola.js
@@ -1,16 +1,15 @@
 const router = require('express').Router()
 const axios = require('axios')
 const {db, TaboolaCampaigns, TaboolaCreatives} = require('../../db')
+//Remember to require in taboola utilities
 
 router.post('/createCampaign', async (req, res, next) => {
   try {
     //Ping Taboola API to create new campaign, send back id or whatever to front
-    console.log('TEST')
     res.sendStatus(201)
   } catch(err) {
       next(err)
   }
 })
-
 
 module.exports = router
---
+++ server/api/campaignManagement/utilities/taboolaAPI.js
@@ -0,0 +1,1 @@
+//Put Taboola API Functions here
