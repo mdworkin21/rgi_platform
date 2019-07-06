57fc03b5ae7bebf52b8f2d9692182488f4355907
Matt Dworkin <mdworkin21@gmail.com>
2019-07-06 17:41:33 -0400

    forcing update to d

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const seed = require('./scripts/seed')
 // Syncs with DB and listens for connections on host and port
 // Put in {force: true} (to db.sync()) to update heroku db
 //Don't use in production, dangerous!
-db.sync({force: true}) 
-  // .then(() => seed()) //uncomment this line for initial seeding comment out/delete after
+db.sync( {force: true}) 
+  .then(() => seed()) //uncomment this line for initial seeding comment out/delete after
   .then(() => {
     console.log('db synced.')
     app.listen(PORT, () => {
