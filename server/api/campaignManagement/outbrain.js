885af37b6c73fd815f77d7ee1052d63d65f487fd
Matt Dworkin <mdworkin21@gmail.com>
2020-02-09 15:13:59 -0500

    herokuuuu

--- server/api/campaignManagement/outbrain.js
+++ server/api/campaignManagement/outbrain.js
@@ -1,11 +1,30 @@
 const router = require('express').Router()
 const axios = require('axios')
 const {uploadToFtp, drop }= require('./utilities/imageManagement')
+const fs = require('fs');
+
 
 router.post('/imageDrop', async(req, res, next) => {
   try {
-    console.log("REQQQQ", req.body)
-    drop(req.body)
+    console.log("REQQQQ", req.body.filePath)
+    let image = '/Users/mdworkin/Downloads' + req.body.filePath  
+    // let newImage = await uploadToFtp(req.body.filePath)
+    let newImage = fs.readFile(image, (err, data) => {
+      //filter out non-ads.txt files
+      try{
+        console.log('FILE IN', data, image);
+        // return files
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        res.write(data);
+        res.end()
+      } catch(e){console.log('E', e)}
+    });
+    await uploadToFtp(newImage);
+    // console.log("NEW", newImage)
     // uploadToFtp(req.body)
+    // res.status(202).send(newImage)
+
   } catch(e) {}
 })
+
+module.exports = router
